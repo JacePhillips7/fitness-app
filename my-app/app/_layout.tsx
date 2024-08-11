7d5c03690e3d18baa10905b0aab6b6ba2dd52c29
jacephillips7 <jacedelynn@gmail.com>
2024-08-10 20:31:18 -0500

    got routes working

---
+++ my-app/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="page2" />
+    </Stack>
+  );
+};
+
+export default Layout;
