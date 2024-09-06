478dbbe3e3b949e6323b35a852492164befe4b52
jacephillips7 <jacedelynn@gmail.com>
2024-09-06 13:57:13 -0500

    fixed some routing, moved login to login

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,9 +1,8 @@
 import { Link, router } from "expo-router";
 import { View, Text, StyleSheet, Button, TextInput } from "react-native";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../firebase.config";
-export default function HomeScreen() {
+export default function IndexScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -12,68 +11,11 @@ export default function HomeScreen() {
     auth.onAuthStateChanged((user) => {
       if (user) {
         router.navigate("home");
+      } else {
+        router.navigate("login");
       }
     });
   });
 
-  const handleLogin = async () => {
-    //set the error to an empty string
-    setError("");
-    try {
-      let userCreds = await signInWithEmailAndPassword(auth, email, password);
-      const user = userCreds.user;
-      await auth.updateCurrentUser(user);
-      //navigate to the dashboard
-      auth.onAuthStateChanged((user) => {
-        router.navigate("home");
-      });
-    } catch (error: any) {
-      const errorMessage = error.message;
-      setError(errorMessage);
-    }
-  };
-
-  const handleSignUp = async () => {
-    //set the error to an empty string
-    setError("");
-    try {
-      let userCreds = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCreds.user;
-    } catch (error: any) {
-      const errorMessage = error.message;
-      setError(errorMessage);
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} />
-
-      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
-
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Sign Up" onPress={handleSignUp} />
-
-      {error ? <Text style={styles.error}>{error}</Text> : null}
-    </View>
-  );
+  return <View></View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    height: 40,
-    width: 200,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  error: {
-    color: "red",
-  },
-});
--- my-app/app/login.tsx
+++ my-app/app/login.tsx
@@ -1,13 +1,68 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { router } from "expo-router";
+import { auth } from "../firebase.config";
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
+export default function login() {
+  const handleLogin = async () => {
+    //set the error to an empty string
+    setError("");
+    try {
+      let userCreds = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCreds.user;
+      await auth.updateCurrentUser(user);
+      //navigate to the dashboard
+      auth.onAuthStateChanged((user) => {
+        router.navigate("home");
+      });
+    } catch (error: any) {
+      const errorMessage = error.message;
+      setError(errorMessage);
+    }
+  };
 
-type Props = {};
+  const handleSignUp = async () => {
+    //set the error to an empty string
+    setError("");
+    try {
+      let userCreds = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCreds.user;
+    } catch (error: any) {
+      const errorMessage = error.message;
+      setError(errorMessage);
+    }
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} />
 
-type State = {};
+      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
 
-export default class Login extends Component<Props, State> {
-  state = {};
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="Sign Up" onPress={handleSignUp} />
 
-  render() {
-    return <div>Login</div>;
-  }
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  error: {
+    color: "red",
+  },
+});
