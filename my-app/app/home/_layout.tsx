6bb7f16202913eb0026f3082412ade91f35847bc
jacephillips7 <jacedelynn@gmail.com>
2024-09-06 17:10:00 -0500

    bring everything up 1 level works

--- my-app/app/home/_layout.tsx
+++ my-app/app/home/_layout.tsx
@@ -1,12 +1,23 @@
-import { Stack } from "expo-router";
-import Tab from ".";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
-const Layout = () => {
+export default function TabLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
   );
-};
-
-export default Layout;
+}
--- my-app/app/home/index.tsx
+++ my-app/app/home/index.tsx
@@ -1,5 +1,36 @@
-import { Redirect } from "expo-router";
+import { Link, router } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+import { auth } from "../../firebase.config";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { useState, useEffect } from "react";
+import Data from "../../components/userdata";
+export default function HomeTab() {
+  const [user, setUser] = useState<User | null>(null);
 
-export default function HomeScreen() {
-  return <Redirect href="/home/(tabs)" />;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) {
+        router.push("login");
+      }
+      return setUser(currentUser);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard</Text>
+      <Text>Welcome {user?.displayName ?? user?.email}</Text>
+      <Link href="/">Login</Link>
+      <Data />
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
