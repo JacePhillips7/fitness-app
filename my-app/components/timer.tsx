857e8449dae5a93485f1783e891a7cecdfa19b6e
jacephillips7 <jacedelynn@gmail.com>
2024-09-12 22:06:44 -0500

    style button

--- my-app/components/timer.tsx
+++ my-app/components/timer.tsx
@@ -1,15 +1,15 @@
 import { useRef, useState } from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
-
-type Props = {};
-
-export default function Timer({}: Props) {
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+const buttonColor = "#295F98";
+export default function Timer() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
   const startTimeRef = useRef<number | null>(null);
 
   const startStopWatch = () => {
+    if (isRunning) return;
     startTimeRef.current = Date.now() - time * 10;
     intervalRef.current = setInterval(() => {
       setTime(Math.floor((Date.now() - startTimeRef.current!) / 10));
@@ -32,14 +32,33 @@ export default function Timer({}: Props) {
     return `${hours ? hours + ":" : ""}${minutes ? minutes + ":" : ""}${seconds}:${centiseconds}`;
   };
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{formatTime(time)}</Text>
       <View style={styles.timerButtons}>
-        <Button title="Start" onPress={startStopWatch} />
-      </View>
-      <View style={styles.timerButtons}>
-        <Button title="Stop" onPress={stopStopWatch} />
-        <Button title="Reset" onPress={resetStopWatch} />
+        {!isRunning ? (
+          <Button
+            mode="contained"
+            buttonColor={buttonColor}
+            onPress={startStopWatch}
+          >
+            Start
+          </Button>
+        ) : (
+          <Button
+            mode="contained"
+            buttonColor={buttonColor}
+            onPress={stopStopWatch}
+          >
+            Stop
+          </Button>
+        )}
+        <Button
+          mode="contained"
+          buttonColor={buttonColor}
+          onPress={resetStopWatch}
+        >
+          Reset
+        </Button>
       </View>
     </View>
   );
@@ -49,5 +68,10 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    alignContent: "center",
+    alignItems: "center",
   },
 });
