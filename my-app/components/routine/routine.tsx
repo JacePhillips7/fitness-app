f4ca95c3162e077ba61f44931bef155615475a71
jacephillips7 <jacedelynn@gmail.com>
2024-09-20 17:46:22 -0500

    better icon button

--- my-app/components/routine/routine.tsx
+++ my-app/components/routine/routine.tsx
@@ -1,16 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { SetStateAction, useState } from "react";
 import { Card, IconButton } from "react-native-paper";
+import { color } from "../../theme/color_theme";
 export interface routine {
   name: string;
   sets: number;
   reps: number;
   complete: boolean;
 }
-const CheckButton = ({ toggleComplete }: { toggleComplete: React.Dispatch<SetStateAction<boolean>> }) => (
+const CheckButton = ({ complete, toggleComplete }: { complete: boolean; toggleComplete: React.Dispatch<SetStateAction<boolean>> }) => (
   <IconButton
     size={24}
-    icon={"check"}
+    icon={complete ? "check" : "checkbox-blank-circle-outline"}
+    iconColor={complete ? "green" : "black"}
     onPress={() => {
       toggleComplete((prev) => !prev);
       console.log("pressed complete button");
@@ -22,7 +24,7 @@ const Routine = ({ routine }: { routine: routine }) => {
   return (
     <View>
       <Card>
-        <Card.Title title={routine.name} right={() => <CheckButton toggleComplete={toggleComplete} />} />
+        <Card.Title title={routine.name} right={() => <CheckButton complete={complete} toggleComplete={toggleComplete} />} />
         <Card.Content>
           <Text>Sets: {routine.sets}</Text>
           <Text>Reps: {routine.reps}</Text>
