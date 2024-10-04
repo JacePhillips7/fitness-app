64fbd7b9ddcf2ad2ef1e776ae8518dcc52c45d7e
jacephillips7 <jacedelynn@gmail.com>
2024-10-04 14:06:30 -0500

    now using the new way of getting the user auth state

--- my-app/app/home/settings.tsx
+++ my-app/app/home/settings.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
-
+import { useAuth } from "../providers/auth.provider";
 export default function Tab() {
+  const { user } = useAuth();
   return (
     <View style={styles.container}>
       <Text>Tab [Settings]</Text>
+      <Text>{user?.displayName ?? user?.email}</Text>
     </View>
   );
 }
